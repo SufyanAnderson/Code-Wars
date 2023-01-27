40f04d4372459423faa9670d18ccb27fdfcef587
yusha7 <sufyananderson@gmail.com>
2023-01-27 14:55:31 -0500

    codewars

---
+++ codewars42/main.js
@@ -0,0 +1,11 @@
+/* 
+need somewhere for the array to be passed in 
+
+
+*/
+
+function findDifference(a, b) {
+    //loading...
+   return Math.abs((a[0] * a[1] * a[2] - b[0] * b[1] * b[2]))
+  
+  }
